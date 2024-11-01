393b9e6cec0f38701bad2d5455813bcde24c8e02
Ndubuaku Emmanuel <ndubuaku.emmanuel@gmail.com>
2024-11-01 03:17:34 +0100

    Try and Fetch

---
+++ fetchLearn.js
@@ -0,0 +1,10 @@
+// Fecth function used for nmaking HTTP request to fecth resources
+fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
+.then(response => {
+    if(!response.ok){
+        throw new Error("Could not fetch data")
+    }
+    return response.json();
+})
+.then(data => console.log(data))
+.catch(error => console.error(error));
